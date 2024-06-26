a93e446b326874c2494b9a20e2a3bfa7e85b008d
littletoxic <1325218784@qq.com>
2024-06-27 01:00:48 +0800

    add ArticleWithExistTag test

--- LTBlog.Test/DatabaseTest.cs
+++ LTBlog.Test/DatabaseTest.cs
@@ -20,7 +20,6 @@ public class DatabaseTest(TestDatabaseFixture fixture) : IClassFixture<TestDatab
             Assert.Equal(1, db.Articles.Count());
         } finally {
             db.Articles.ExecuteDelete();
-            db.SaveChanges();
         }
     }
 
@@ -47,7 +46,6 @@ public class DatabaseTest(TestDatabaseFixture fixture) : IClassFixture<TestDatab
         } finally {
             db.Articles.ExecuteDelete();
             db.Tags.ExecuteDelete();
-            db.SaveChanges();
         }
     }
 
@@ -64,7 +62,58 @@ public class DatabaseTest(TestDatabaseFixture fixture) : IClassFixture<TestDatab
             Assert.Equal(1, db.Tags.Count());
         } finally {
             db.Tags.ExecuteDelete();
-            db.SaveChanges();
+        }
+    }
+
+    [Fact]
+    public void AddTagWithArticle() {
+        var article = new Article {
+            Title = "Test Article",
+            Tags = [],
+            Content = "This is a test article."
+        };
+        db.Attach(article);
+        db.SaveChanges();
+
+        var tag = new Tag {
+            Name = "Test Tag",
+            Articles = [article]
+        };
+        db.Attach(tag);
+        db.SaveChanges();
+
+        try {
+            Assert.Equal(1, db.Articles.Count());
+            Assert.Equal(1, db.Tags.Count());
+        } finally {
+            db.Articles.ExecuteDelete();
+            db.Tags.ExecuteDelete();
+        }
+    }
+
+    [Fact]
+    public void ArticleWithExistTag() {
+        var tag = new Tag {
+            Name = "Test Tag",
+            Articles = []
+        };
+        db.Attach(tag);
+        db.SaveChanges();
+
+        var article = new Article {
+            Title = "Test Article",
+            Tags = [.. db.Tags.Where(t => t.Name == "Test Tag")],
+            Content = "This is a test article."
+        };
+        db.Attach(article);
+        db.SaveChanges();
+
+        try {
+            Assert.Equal(1, db.Articles.Count());
+            Assert.Equal(1, db.Tags.Count());
+        } finally {
+            db.Articles.ExecuteDelete();
+            db.Tags.ExecuteDelete();
         }
     }
 }
@@ -73,11 +122,12 @@ public sealed class TestDatabaseFixture : IDisposable {
 
     public TestDatabaseFixture() {
         var options = new DbContextOptionsBuilder<ArticleContext>()
-            .UseNpgsql("Host=localhost;Database=test;Username=postgres;Password=&0k42^V2EOD*AjS")
+            .UseNpgsql("Host=localhost;Database=test;Username=postgres;Password=&0k42^V2EOD*AjS;Include Error Detail=true")
             .UseSnakeCaseNamingConvention()
             .Options;
         Context = new(options);
 
+        Context.Database.EnsureDeleted();
         Context.Database.EnsureCreated();
     }
 
