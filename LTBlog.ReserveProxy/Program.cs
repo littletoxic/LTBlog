c96b9b9eb7e3b2ef13b57d3809bc6801beb58261
littletoxic <1325218784@qq.com>
2024-09-04 19:11:59 +0800

    配置反代，增加证书配置

--- LTBlog.ReserveProxy/Program.cs
+++ LTBlog.ReserveProxy/Program.cs
@@ -1,6 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+builder.Services.AddLettuceEncrypt();
+
+builder.WebHost.ConfigureKestrel(kestrel => {
+    kestrel.ConfigureHttpsDefaults(options => {
+        options.UseLettuceEncrypt(kestrel.ApplicationServices);
+    });
+});
+
 var app = builder.Build();
+app.UseHttpsRedirection();
 app.MapReverseProxy();
 await app.RunAsync();
