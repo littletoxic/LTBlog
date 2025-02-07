85154f5e97fa2b724fcf01303ce5fb74b0210219
littletoxic <1325218784@qq.com>
2025-02-08 06:16:58 +0800

    完成代理的迁移

--- src/ReverseProxy/Program.cs
+++ src/ReverseProxy/Program.cs
@@ -1,6 +1,31 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+
+var builder = WebApplication.CreateBuilder(new WebApplicationOptions {
+    Args = args,
+    // 保证开发与发布行为一致
+    ContentRootPath = AppContext.BaseDirectory
+});
+
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+builder.Services.AddLettuceEncrypt();
+
+builder.WebHost.ConfigureKestrel(kestrel => {
+    kestrel.ConfigureHttpsDefaults(options => {
+        options.UseLettuceEncrypt(kestrel.ApplicationServices);
+    });
+    kestrel.ListenAnyIP(80);
+    kestrel.ListenAnyIP(443, options => {
+        options.Protocols = HttpProtocols.Http1AndHttp2;
+        options.UseHttps();
+    });
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapReverseProxy();
 
-app.Run();
+app.MapStaticAssets("Client.staticwebassets.endpoints.json");
+app.MapFallbackToFile("index.html");
+
+await app.RunAsync();
