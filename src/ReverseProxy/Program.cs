68f779068ca8a85d308f5adeb414023fde168943
littletoxic <1325218784@qq.com>
2025-02-08 22:55:16 +0800

    修改配置文件方便开发

--- src/ReverseProxy/Program.cs
+++ src/ReverseProxy/Program.cs
@@ -1,5 +1,3 @@
-using Microsoft.AspNetCore.Server.Kestrel.Core;
-
 var builder = WebApplication.CreateBuilder(new WebApplicationOptions {
     Args = args,
     // 保证开发与发布行为一致
@@ -13,11 +11,6 @@ builder.Services.AddLettuceEncrypt();
 builder.WebHost.ConfigureKestrel(kestrel => {
     kestrel.ConfigureHttpsDefaults(options => {
         options.UseLettuceEncrypt(kestrel.ApplicationServices);
-    });
-    kestrel.ListenAnyIP(80);
-    kestrel.ListenAnyIP(443, options => {
-        options.Protocols = HttpProtocols.Http1AndHttp2;
-        options.UseHttps();
     });
 });
 
